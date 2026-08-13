7d6f0ae4ea59a6720a4253290e0e546bb2f7b6b9
Thusyya-Vardhan <thusyya2006@gmail.com>
2026-08-13 18:48:04 +0530

    hiding navbar when onboarding

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -1,4 +1,4 @@
-import { Outlet, useLoaderData, useRouteError } from "react-router";
+import { Outlet, useLoaderData, useRouteError, useLocation } from "react-router";
 import { boundary } from "@shopify/shopify-app-react-router/server";
 import { AppProvider } from "@shopify/shopify-app-react-router/react";
 import { authenticate } from "../shopify.server";
@@ -13,8 +13,12 @@ export const loader = async ({ request }) => {
 export default function App() {
   const { apiKey } = useLoaderData();
 
+  const location = useLocation();
+  const showNav = location.pathname !== "/app/onboarding";
+
   return (
     <AppProvider embedded apiKey={apiKey}>
+      { showNav && 
       <s-app-nav>
         {/* <s-link href="/app">Home</s-link> */}
         <s-link href="/app/overview">Overview</s-link>
@@ -26,7 +30,8 @@ export default function App() {
         <s-link href="/app/settings">Settings</s-link>
         {/* <s-link href="/app/additional">Additional page</s-link> */}
       </s-app-nav>
-      <Outlet />
+}
+     <Outlet />
     </AppProvider>
   );
 }
