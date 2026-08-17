09bb64c678ad1219e2e5da1143225a0307dfb969
Thusyya-Vardhan <thusyya2006@gmail.com>
2026-08-17 20:01:04 +0530

    onboarding routing fix

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -2,5 +2,5 @@ import { redirect } from "react-router";
 
 export const loader = async ({request}) => {
   const url = new URL(request.url);
-   return redirect(`/app/overview?${url.searchParams.toString()}`);
+   return redirect(`/app/onboarding?${url.searchParams.toString()}`);
 }
